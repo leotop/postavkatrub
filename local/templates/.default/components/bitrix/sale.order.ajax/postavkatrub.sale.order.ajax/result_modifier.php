c88a81e9135aec3624137ce5568b7570822b0d28
Admin <git@webgk.ru>
2016-10-26 11:18:53 +0300

    SUP-4988: if quantity < 0 then not show Sberbank in basket

--- local/templates/.default/components/bitrix/sale.order.ajax/postavkatrub.sale.order.ajax/result_modifier.php
+++ local/templates/.default/components/bitrix/sale.order.ajax/postavkatrub.sale.order.ajax/result_modifier.php
@@ -1,1 +1,15 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?
+    $GLOBALS["ShowSberbank"] = 'Y';
+    foreach($arResult['BASKET_ITEMS'] as $arBasketItem){
+        $arBasketItemID[] = $arBasketItem["PRODUCT_ID"];
+    };
+    $arSelect = Array("CATALOG_QUANTITY");
+    $arFilter = Array("ID" => $arBasketItemID);
+    $res = CIBlockElement::GetList(Array(), $arFilter, false, Array("nPageSize"=>50), $arSelect);
+    while ($arFields = $res->Fetch()) {
+        if ($arFields["CATALOG_QUANTITY"] == 0) {
+            $GLOBALS["ShowSberbank"] = "N";       
+        }    
+    }
+?>
